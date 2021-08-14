e05f3c017e78a19975fd2855728257383b2074c8
jpabloroa <jpablonano@gmail.com>
2021-08-13 21:14:53 -0500

    MacroRecorder V 1.0 -> Roverin Technologics

--- src/grabador/Accion.java
+++ src/grabador/Accion.java
@@ -6,94 +6,249 @@
 package grabador;
 
 import java.awt.*;
-import static java.awt.event.KeyEvent.*;
 import java.awt.event.*;
+import java.awt.image.*;
+import java.io.IOException;
+import java.util.*;
+import javax.swing.*;
 
 /**
  *
  * @author Juan Pablo - Roverin Technologics
  */
-public class Accion implements MouseListener {
+public class Accion extends Interactor implements MouseListener, KeyListener {
+
+    /**
+     *
+     * @param panel
+     * @param imagen
+     * @param Nombre_Macro
+     * @param process
+     * @throws AWTException
+     * @throws java.io.IOException
+     */
+    public Accion(String Nombre_Macro, JFrame panel, JLabel imagen, int process) throws AWTException, IOException {
+        super(Nombre_Macro, process);
+        this.error = new StringBuffer();
+        this.report = new StringBuffer();
+        this.cmd = new StringBuffer();
+        this.robot = new Robot();
+        this.screen = Toolkit.getDefaultToolkit().getScreenSize();
+        this.screenRect = new Rectangle(this.screen);
+        this.panel = panel;
+        this.imagen = imagen;
+        imagen.setIcon(new ImageIcon(getScaledImage()));
+    }
+
+    /**
+     *
+     * @return @throws IOException
+     */
+    public String performErel() throws IOException {
+        super.leerInstruccion();
+        return "done";
+    }
+
+    /**
+     *
+     * @param comd
+     */
+    public void newRecord(String comd) {
+//        if (isRecording) {
+//            String op = stopTimer() + comd;
+//            super.grabar(comd);
+//            startTimer();
+//        }
+
+    }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        a = MouseInfo.getPointerInfo();
-        b = a.getLocation();
-        b = a.getLocation();
-        x = b.getX();
-        y = b.getY();
-        System.out.println("Posición y: " + y + " ---- Posición x: " + x);
-        try {
-            robot = new Robot();
-        } catch (AWTException ex) {
-            mensaje += " ¡ Error en la generación del Bot ! - Detalles : " + ex.getMessage() + "\n";
-            newErrorMessages();
-        }
-        changeWindow();
-        doClick();
-        changeWindow();
+        int btnmsk = MouseEvent.getMaskForButton(e.getButton());
+        int x = e.getX();
+        int y = e.getY();
+        this.panel.setVisible(false);
+        double[] res = obtain(e.getX(), e.getY());
+        robot.mouseMove((int) res[0], (int) res[1]);
+        robot.mousePress(btnmsk);
+        robot.delay(100);
+        robot.mouseRelease(btnmsk);
+        robot.delay(150);
+        this.panel.setVisible(true);
+        robot.mouseMove(e.getXOnScreen(), e.getYOnScreen());
+        imagen.setIcon(new ImageIcon(getScaledImage()));
+        super.ew.addClickEvent("do", e.getButton(), (int) res[0], (int) res[1]);
+        super.ew.addClickEvent("stop", e.getButton(), (int) res[0], (int) res[1]);
+        report.append(Calendar.getInstance().getTime().toString()).append(" ¡ Nuevo evento registrado ! - > click: ").append(e.getButton()).append("\n");
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        int keycode = KeyEvent.getExtendedKeyCodeForChar(e.getKeyChar());
+        this.panel.setVisible(false);
+        robot.keyPress(keycode);
+        robot.delay(100);
+        robot.keyRelease(keycode);
+        this.panel.setVisible(true);
+        imagen.setIcon(new ImageIcon(getScaledImage()));
+        super.ew.addKeyEvent("do", keycode);
+        super.ew.addKeyEvent("stop", keycode);
+        report.append(Calendar.getInstance().getTime().toString()).append(" ¡ Nuevo evento registrado ! - > char: ").append(e.getKeyChar()).append("\n");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    /**
+     *
+     * @param opc
+     * @throws java.awt.AWTException
+     */
+    public void funcAct(int opc) throws AWTException {
+        int key = 0;
+        int k1 = 0, k2 = 0;
+        switch (opc) {
+            case 1:
+                key = KeyEvent.VK_COPY;
+                k1 = KeyEvent.VK_CONTROL;
+                k2 = KeyEvent.VK_C;
+                break;
+            case 2:
+                key = KeyEvent.VK_PASTE;
+                k1 = KeyEvent.VK_CONTROL;
+                k2 = KeyEvent.VK_V;
+                break;
+            default:
+                throw new AWTException(" ¡ La función especificada no es válida ! ");
+        }
+        this.panel.setVisible(false);
+        robot.keyPress(key);
+        robot.keyRelease(key);
+        this.panel.setVisible(true);
+        imagen.setIcon(new ImageIcon(getScaledImage()));
+        newRecord(setCntrComd(k1, k2, 100));
+        report.append(Calendar.getInstance().getTime().toString()).append(" ¡ Nuevo evento registrado ! - > char: ").append(KeyEvent.getExtendedKeyCodeForChar(KeyEvent.VK_COPY)).append("\n");
+    }
+
+    private String setCntrComd(int Key1, int Key2, int wait) {
+        return new StringBuffer().append("do key press -").append(KeyEvent.getExtendedKeyCodeForChar(Key1)).append("; ").append("do key press -").append(KeyEvent.getExtendedKeyCodeForChar(Key2)).append("; ").append("do wait -").append(wait).append("; ").append("stop key press -").append(KeyEvent.getExtendedKeyCodeForChar(Key1)).append("; ").append("stop key press -").append(KeyEvent.getExtendedKeyCodeForChar(Key2)).append("; ").toString();
+    }
+
+    /**
+     *
+     * @param e
+     * @param x
+     * @param y
+     * @return
+     */
+    private String setMouseCmd(MouseEvent e, int x, int y, int wait) {
+        return new StringBuffer().append("do click press -").append(e.getButton()).append(" at -").append(x).append(" -").append(y).append("; ").append("do wait -").append(wait).append("; ").append("stop click press -").append(e.getButton()).append(" at -").append(x).append(" -").append(y).append("; ").toString();
 
     }
 
-    private void makeDelay(int MiliSegundos) {
-        robot.delay(MiliSegundos);
+    /**
+     *
+     * @param e
+     * @param x
+     * @param y
+     * @return
+     */
+    private String setKeyCmd(KeyEvent e, int wait) {
+        return new StringBuffer().append("do key press -").append(e.getKeyChar()).append("; ").append("do wait -").append(wait).append("; ").append("stop key press -").append(e.getKeyChar()).append("; ").toString();
     }
 
-    private void doClick() {
-        makeDelay(500);
-        robot.mousePress(BUTTON1_MASK);
-        robot.mouseRelease(BUTTON1_MASK);
-        makeDelay(500);
+    /**
+     *
+     * @param x
+     * @param y
+     * @return
+     * @throws AWTException
+     */
+    private double[] obtain(int x, int y) {
+        double propx = x / (screen.getWidth() * prop);
+        double propy = y / (screen.getHeight() * prop);
+
+        double intx = screen.getWidth() * propx;
+        double inty = screen.getHeight() * propy;
+
+        return new double[]{intx, inty};
     }
 
-    private void changeWindow() {
-        robot.keyPress(VK_ALT);
-        robot.keyPress(VK_TAB);
-        robot.keyRelease(VK_ALT);
-        robot.keyRelease(VK_TAB);
+    /**
+     *
+     * @return @throws AWTException
+     */
+    private Image getScaledImage() {
+        Dimension dim = this.panel.getSize();
+        this.panel.setSize(0, 0);
+        try {
+            this.imageBuffer = new Robot().createScreenCapture(screenRect);
+        } catch (AWTException ex) {
+            error.append(Calendar.getInstance().getTime().toString()).append(" -> ¡ Nuevo error en la instrucción getScaledImage ! \n");
+        }
+        this.panel.setSize(dim);
+        this.h = this.imageBuffer.getHeight() * prop;
+        this.w = this.imageBuffer.getWidth() * prop;
+        //----------------------------------------------------------------------
+        resizedImg = new BufferedImage((int) this.w, (int) this.h, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = resizedImg.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2.drawImage(imageBuffer, 0, 0, (int) this.w, (int) this.h, null);
+        g2.dispose();
+        return resizedImg;
     }
 
-    public String returnErrorMessage() {
-        return mensaje;
+    /**
+     *
+     * @return
+     */
+    public String getErrorMessages() {
+        return error.toString();
     }
 
-    private void newErrorMessages() {
-        val = true;
+    /**
+     *
+     * @return
+     */
+    public String getEventMessages() {
+        return report.toString();
     }
 
-    public boolean newErrorMessage() {
-        boolean result = val;
-        val = false;
-        return result;
-    }
+    /**
+     *
+     */
+    public double h, w;
 
-    private String nombre;
-    private String descripcion;
-    private MouseEvent evento;
-    private PointerInfo a;
-    private Point b;
-    private double x, y;
-    private Robot robot;
-    private String mensaje;
-    private boolean val;
-
+    /**
+     *
+     */
+    private final JFrame panel;
+    private final JLabel imagen;
+    private final StringBuffer error, report, cmd;
+    private final double prop = 0.5;
+    private final Robot robot;
+    private final Dimension screen;
+    private final Rectangle screenRect;
+    private BufferedImage imageBuffer, resizedImg;
 }
---
+++ src/grabador/Interactor.java
@@ -0,0 +1,201 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grabador;
+
+import java.io.IOException;
+import erel.Compiler.ErelWriter;
+import erel.Compiler.ErelLoader;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Juan Pablo - Roverin Technologics
+ */
+public class Interactor /*implements MouseListener, KeyListener*/ {
+
+    /**
+     * 
+     */
+    public Interactor(){        
+    }
+    /**
+     *
+     * @param NombreMacro
+     * @param PROCESS
+     * @throws IOException
+     * @throws AWTException
+     */
+    public Interactor(String NombreMacro, int PROCESS) throws IOException, AWTException {
+        if (NombreMacro != null) {
+            switch (PROCESS) {
+                case 101:
+            this.ew = new ErelWriter(modify("/dimRest", "/src/dimRest/dll-exec-x68-" + formatString(Calendar.getInstance().getTime().toString(), 1) + formatString(NombreMacro, 1) + ".erel"));
+                    this.er = null;
+            new Thread(this.ew).start();
+                    break;
+                case 102:
+                    this.ew = null;
+            this.er = new ErelLoader(modify("/dimRest", "/src/dimRest/dll-exec-x68-" + NombreMacro + ".erel"), new Robot());
+                    break;
+                default:
+                    throw new IOException(" ¡ El proceso a realizar no se encuentra !");
+            }
+        } else {
+            throw new IOException(" ¡ El nombre del archivo es nulo !");
+        }
+    }
+
+//    /**
+//     *
+//     * @param ch
+//     * @param Command
+//     */
+//    public void grabar(int ch, String Command) {
+//        switch (ch) {
+//            case 1:
+//                ew.addClickEvent(Command, ch, ch, ch);
+//                break;
+//            case 2:
+//                
+//                break;
+//        }
+//    }
+    /**
+     *
+     * @return @throws IOException
+     */
+    public String generarInstrucccion() throws IOException {
+        ew.writeFile();
+        return "cmd add correctly";
+    }
+
+    /**
+     *
+     * @param Value
+     * @param Option
+     * @return
+     */
+    private String formatString(String Value, int Option) {
+        if (Option == 1 || Option == 2) {
+            String val = Value;
+            String[] ch = new String[val.length()];
+            for (int index = 0; index < Value.length(); index++) {
+                int p = index;
+                if (p == Value.length()) {
+                    ch[index] = val.substring(p);
+                } else {
+                    ch[index] = val.substring(p, p + 1);
+                }
+            }
+            if (Option == 1) {
+                for (int index = 0; index < Value.length(); index++) {
+                    switch (ch[index]) {
+                        case " ":
+                            ch[index] = "_";
+                            break;
+                        case "/":
+                            ch[index] = "-";
+                            break;
+                        case ":" , "*",  "?" , "<", ">", "|":
+                            ch[index] = ".";
+                            break;
+                    }
+                }
+            } else if (Option == 2) {
+                for (int index = 0; index < Value.length(); index++) {
+                    switch (ch[index]) {
+                        case "_":
+                            ch[index] = " ";
+                            break;
+                        case "-":
+                            ch[index] = " / ";
+                            break;
+                        case "&":
+                            ch[index] = " -> ";
+                            break;
+                    }
+                }
+            }
+            String result = String.join("", ch);
+            return result;
+        }
+        return " ¡ Error de formateo !";
+    }
+
+    /**
+     * 
+     * @param filename
+     * @param un
+     * @return 
+     */
+    public String formatName(String filename, int un) {
+        switch (un) {
+            case 1:
+                String pal = formatString(filename, FROM_FORMAT);
+                int larg = pal.length();
+                return pal.substring(19, 47) + " -> " + pal.substring(47, larg - 5);
+            case 2:
+                return formatString(filename.substring(0, 28) + filename.substring(32), TO_FORMAT);
+        }
+        return null;
+    }
+    
+    /**
+     * 
+     * @param ref
+     * @param dic
+     * @return 
+     */
+    public String modify(String ref, String dic) {
+        
+        String aux = getClass().getResource(ref).getFile().substring(1, 74) + dic;
+        
+        StringBuffer hud = new StringBuffer();
+
+        for (int i = 0; i < aux.length(); i++) {
+            String tas = aux.substring(i, i + 1);
+            switch (tas) {
+                case "/":
+                    hud.append("\\");
+                    break;
+                case "%":
+                    hud.append(" ");
+                    i++;
+                    i++;
+                    break;
+                default:
+                    hud.append(tas);
+                    break;
+            }
+        }
+        return hud.toString();
+    }
+
+    /**
+     *
+     */
+    public void leerInstruccion() {
+        er.readFile();
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public int tiempoEjec(){
+        return er.getTimeOfSteps();
+    } 
+
+    //
+    public int TO_FORMAT = 1;
+    public int FROM_FORMAT = 2;
+    public static int WRITE_EREL_FILE = 101;
+    public static int READ_EREL_FILE = 102;
+    public ErelWriter ew;
+    public ErelLoader er;
+}
